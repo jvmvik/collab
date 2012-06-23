3d35488db5bcac392b0eb96cb8edb1409f46325d
Victor Benarbia <jvmvik@gmail.com>
2012-06-23 17:51:02 -0500

    Try to enable button to switch between view

--- src/com/arm/nimbus/collab/client/navigation/Navigation.java
+++ src/com/arm/nimbus/collab/client/navigation/Navigation.java
@@ -1,7 +1,12 @@
 package com.arm.nimbus.collab.client.navigation;
 
+import com.arm.nimbus.collab.client.dashboard.DashboardPlace;
+import com.arm.nimbus.collab.client.editor.TaskBoardPlace;
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.place.shared.PlaceController;
 import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.HTMLPanel;
 import com.google.inject.Inject;
@@ -17,6 +22,9 @@ public class Navigation extends Composite {
     @Inject
     EventBus eventBus;
 
+    @Inject
+    PlaceController placeController;
+
     interface NavigationUiBinder extends UiBinder<HTMLPanel, Navigation> {
     }
 
@@ -25,5 +33,16 @@ public class Navigation extends Composite {
     public Navigation() {
         HTMLPanel navbar = ourUiBinder.createAndBindUi(this);
         initWidget(navbar);
+    }
+
+    @UiHandler("product")
+    public void product(ClickEvent event){
+        GWT.log("Select product");
+          placeController.goTo(new DashboardPlace());
+    }
+
+    @UiHandler("tasks")
+    public void tasks(ClickEvent event){
+        placeController.goTo(new TaskBoardPlace());
     }
 }
